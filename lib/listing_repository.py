020eee5be659cb66bc809b8eb0c471055bd6c03b
Onuora <onuora.wright@gmail.com>
2023-10-25 12:37:07 +0100

    Test driven Listing Repository

--- lib/listing_repository.py
+++ lib/listing_repository.py
@@ -12,17 +12,36 @@ class ListingRepository():
             listings.append(listing)
         return listings
     
-    def generate_single_listing(self, rows):
-        pass
+    # generate a single listing from a SQL query row
+    def generate_single_listing(self, rows) -> Listing:
+        if rows == []:
+            return None
+        row = rows[0]
+        return Listing(row['id'], row['title'], row['description'], row['price'], row['owner_id'])
+    
         
-    def create(self):
-        pass
+    def create(self, title, description, price, owner_id):
+        query = 'INSERT INTO listings (title, description, price, owner_id) VALUES (%s, %s, %s, %s) RETURNING id'
+        params = [title, description, price, owner_id]
+        rows = self._connection.execute(query, params)
+        listing_id = rows[0]['id']
+        return listing_id
 
-    def find(self):
-        pass
+    def find(self, listing_id:int) -> Listing:
+        query = 'SELECT * FROM listings WHERE id=%s'
+        params = [listing_id]
 
-    def delete(self):
-        pass
+        rows = self._connection.execute(query, params)
+        return self.generate_single_listing(rows)
+
+  # == DELETE A LISTING =============
+
+    # Delete a listing by id
+    def delete(self, listing_id:int) -> Listing:
+        query = 'DELETE FROM listings WHERE id = %s'
+        params = [listing_id]
+        self._connection.execute(query, params)
+        return None
 
     def all(self): # Like find but showing everything in the table
         query = 'SELECT * FROM listings'
